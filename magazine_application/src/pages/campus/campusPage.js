d9b3581a4c6adb00e4018e19337130d57c0529e9
rowdysquirrel23 <24904759@edgehill.ac.uk>
2024-04-03 17:05:44 +0100

    Campus news page content added + basic styling

--- magazine_application/src/pages/campus/campusPage.js
+++ magazine_application/src/pages/campus/campusPage.js
@@ -1,11 +1,12 @@
 import './campus.css';
 import story1 from '../../assets/su_building.jpg';
+import story2 from '../../assets/trophy.jpg';
+import story3 from '../../assets/goose_story.jpg';
 
 export default function Campus(){
     return (
         <body className="campus_body">
             <h1 className="campus_header">Campus News!</h1>
-
             <section className="articles">
                 <div className="story" >
                     <h2 className="story1title">New Student Union</h2>
@@ -16,11 +17,21 @@ export default function Campus(){
                 </div>
                 <div className="story">
                     <h2 className="story2title">Modern University Again?</h2>
-                    <p>text</p>
+                    
+                    <p className="story2text">Edge Hill University is on a mission to reclaim its title as the Modern University of the Year, driven by a relentless pursuit of innovation and excellence. 
+                        With a commitment to pushing boundaries in teaching, research, and student support, the university is actively shaping the future of higher education. 
+                        Embracing cutting-edge technology, fostering interdisciplinary collaboration, and prioritizing the student experience, 
+                        Edge Hill is poised to once again stand at the forefront of modern education. 
+                        </p>
+                        <img src={story2} className="story2img"></img>
                 </div>
                 <div className="story">
-                    <h2 className="story3title">title</h2>
-                    <p>text</p>
+                    <h2 className="story3title">Geese Taking Over!</h2>
+                    <img src={story3} className="story3img"></img>
+                    <p className="story3text">Canadian geese have become a nuisance for students at Edge Hill University,
+                        creating a stir with their aggressive behavior on campus grounds. These feathered intruders, typically known for their graceful presence,
+                        have taken to asserting dominance in certain areas, causing concern among students and faculty alike. 
+                    </p>
                 </div>
             </section>
         </body>
