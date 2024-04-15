80dae6e853b276fd5f8b29ada1d169bb7a995f61
rowdysquirrel23 <24904759@edgehill.ac.uk>
2024-04-15 23:09:13 +0100

    Home page comments added

--- magazine_application/src/pages/home/homePage.js
+++ magazine_application/src/pages/home/homePage.js
@@ -13,12 +13,14 @@ export default function Home() {
 
 
     return (
-            <body>
+        // div to contain the whole page content
+            <div className='home_body'>
+                {/* This section contains the banner image and hero text that is placed on top of it */}
                 <section className="banner_section">
                     <img className="hero_image" src={banner_img}></img>
                     <h1 className="banner_hero">EDGE EXPRESS</h1>
                 </section>
-
+                {/* General title and text about Edge Hill University */}
                 <h2 className="general_info">Edge Hill News</h2>
                     <p className="general_text">
                         In 2022, Edge Hill University clinched the prestigious title of Modern University of the Year.
@@ -27,37 +29,42 @@ export default function Home() {
                         This recognition not only reaffirms Edge Hill's position as a leader in higher education but also underscores 
                         its ongoing efforts to provide students with an unparalleled educational experience supported by up-to-date facilities.
                     </p>
+                {/* This section contains image links to the rest of the site, the home page is used as a 'contents' page you would see in a magazine  */}
                 <section className="home_content">
+                    {/* A tag to allow both the text and image to be used as a link */}
                     <a href='/campus'>
-                        <img src={img1} className="library_image" alt="Image of a library"></img>
+                        <img src={img1} className="library_image" alt="a library"></img>
                         <p className='image_text'>View our Campus News!</p>
                     </a>
                     <a href='/sport'>
-                        <img src={img2} className="football_image" alt="Image of a football game"></img>
+                        <img src={img2} className="football_image" alt="a football game"></img>
                         <p className='image_text'>View our Sports News!</p>
                     </a>
                     <a href='/activities'>
-                        <img src={img3} className="activity_image" alt="Image of a bowling ball"></img>
+                        <img src={img3} className="activity_image" alt="a bowling ball"></img>
                         <p className='image_text'>View local Activities!</p>
                     </a>
                 </section>
+                {/* Section to promote the websites social media pages */}
                 <h3 className="social_title">Vist Our Social Media's!</h3>
                 <section className="social_media">
                     <div className="social_links">
+                        {/* Users can click the icon to be taken to the socil media */}
                         <a href="https://www.snapchat.com/">
-                        <img src={img4} className="social_icon" alt="Image of the Snapchat icon"></img>
+                        <img src={img4} className="social_icon" alt="the Snapchat icon"></img>
                         </a>
 
                         <a href="https://www.instagram.com/">
-                        <img src={img5} className="social_icon" alt="Image of the Instagram Icon"></img>
+                        <img src={img5} className="social_icon" alt="the Instagram Icon"></img>
                         </a>
 
                         <a href="https://www.facebook.com/">
-                        <img src={img6} className="social_icon" alt="Image of the Facebook icon"></img>
+                        <img src={img6} className="social_icon" alt="the Facebook icon"></img>
                         </a>
                     </div>
+                    {/* small text description encouraging users to visit social media */}
                     <p className="social_description">Visit us on Social Media to keep up to date with all things Edge Hill.</p>
                 </section>
-            </body>
+            </div>
     )
 }
