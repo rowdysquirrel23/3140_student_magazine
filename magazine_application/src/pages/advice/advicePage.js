a54f48e6f2afa402a0cc9834db9d65c30e085209
rowdysquirrel23 <24904759@edgehill.ac.uk>
2024-04-16 00:03:25 +0100

    Comments added to advice page

--- magazine_application/src/pages/advice/advicePage.js
+++ magazine_application/src/pages/advice/advicePage.js
@@ -1,3 +1,4 @@
+// import stylesheets and libraries
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import './advice.css';
@@ -5,27 +6,29 @@ import './advice.css';
 
 export default function Advice(){
   
-
+// set the peramters for the api
   const [form_data, set_form_data] = useState({ name: '', comment: '' });
   const [submitted_data, set_submitted_data] = useState([]);
-
+// handle the change of data
   const handle_change = (e) => {
     const { name, value } = e.target;
     set_form_data({ ...form_data, [name]: value });
   };
-
+// Handle the submission of data to the api
   const handle_submit = (e) => {
     e.preventDefault();
+    // post to the api using axios library
     axios.post('/api', form_data)
       .then(response => {
         set_submitted_data([...submitted_data, form_data]);
         set_form_data({ name: '', comment: '' });
       })
+      // Check for errors
       .catch(error => {
         console.error('Error submitting form:', error);
       });
   };
-
+// Call the submitted data from the api
   useEffect(() => {
     axios.get('/api')
       .then(response => {
@@ -38,11 +41,11 @@ export default function Advice(){
 
   return (
 
-    
     <div className='advice_body'>
       <h1 className='advice_title'>Student Confessions and Problems!</h1>
+      {/* Section of problems students face */}
       <section className='student_problems'>
-
+        {/* Section of a students problem */}
         <div className='student_submissions'>
           <h3 className='student_details'>Jake, 19</h3>
           <p className='student_story'>
@@ -51,7 +54,7 @@ export default function Advice(){
           I've tried talking to them about it, but it seems like they just can't resist the temptation of my tasty treats.
           </p>
         </div>
-
+        {/* Section of a students problem */}
         <div className='student_submissions'>
           <h3 className='student_details'>Toby, 20</h3>
           <p className='student_story'>
@@ -59,7 +62,7 @@ export default function Advice(){
           While the proximity to classes and campus amenities is appealing, the allure of having my own space and more flexibility off-campus is equally enticing.
           </p>
         </div>
-
+        {/* Section of a students problem */}
         <div className='student_submissions'>
           <h3 className='student_details'>Lucy, 18</h3>
           <p className='student_story'>
@@ -68,15 +71,18 @@ export default function Advice(){
           </p>
         </div>
       </section>
+      {/* Section for back end entry for the site */}
       <section className='add_comment'>
       <h2 className='comments_title'>Student Opinions!:</h2>
         <ul className='display_comments'>
+          {/* Recall the data from the api as a comment section */}
           {submitted_data.map((data, index) => (
             <li key={index}>
               <strong>Name:</strong> {data.name}, <strong>Advice:</strong> {data.comment}
             </li>
           ))}
         </ul>
+        {/* form for users to submit their opiniions to a json server */}
         <h2 className='comments_instructions'>Comment Your Opinion!</h2>
         <form onSubmit={handle_submit} className='comment_form'>
           <label>
@@ -87,6 +93,7 @@ export default function Advice(){
             Advice:
             <input type="text" className="comment_box" name="comment" value={form_data.comment} onChange={handle_change} />
           </label>
+          {/* Button submits the user feedback to the json server */}
           <button type="submit">Submit</button>
         </form>
       </section>
