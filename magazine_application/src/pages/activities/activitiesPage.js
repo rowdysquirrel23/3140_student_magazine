9fc66e3b8de1553b638b3cc0d322490fac0778a8
rowdysquirrel23 <24904759@edgehill.ac.uk>
2024-04-15 23:50:21 +0100

    Comments added for activities page

--- magazine_application/src/pages/activities/activitiesPage.js
+++ magazine_application/src/pages/activities/activitiesPage.js
@@ -1,3 +1,4 @@
+// Import stylesheet and images
 import './activities.css';
 import activities1 from '../../assets/pubgolf.jpg';
 import pub1 from '../../assets/pub_list/pub1.jpg';
@@ -13,91 +14,86 @@ import pub9 from '../../assets/pub_list/pub9.jpg';
 
 export default function Activities(){
     return (
-        <body>
+        <div>
             <h1 className="activities_header">ACTIVITIES</h1>
+            {/* Image with text overlay */}
             <section className="activities_banner">
                 <h2 className="golf_title"> Pub Golf</h2>
                 <img src={activities1} className="pub_golf_img"></img>
             </section>
 
             <h2 className="activities_instructions">Pubs and Holes</h2> 
+            {/* Section for the cards of pubs */}
             <section className="pub_list">
+                
                 <div className="pub_card">
+                    {/* a pub card, image and text description */}
                     <article className="pub_section">
                         <img src={pub1} className="pub_img" alt="Image of the front of a pub"></img>
-
                         <div className="pub_info">
                             <h3 className="pub_number">Pub 1</h3>
                             <p className="pub_name">The Rose & Crown</p>
                         </div>
                     </article>
-
+                    {/* a pub card, image and text description */}
                     <article className="pub_section">
                         <img src={pub2} className="pub_img" alt="Image of the front of a pub"></img>
-
                         <div className="pub_info">
                             <h3 className="pub_number">Pub 2</h3>
                             <p className="pub_name">The Horseshoe</p>
                         </div>
                     </article>
-
+                    {/* a pub card, image and text description */}
                     <article className="pub_section">
                         <img src={pub3} className="pub_img" alt="Image of the front of a pub"></img>
-
                         <div className="pub_info">
                             <h3 className="pub_number">Pub 3</h3>
                             <p className="pub_name">The Railway</p>
                         </div>
                     </article>
-
+                    {/* a pub card, image and text description */}
                     <article className="pub_section">
                         <img src={pub4} className="pub_img" alt="Image of the front of a pub"></img>
-
                         <div className="pub_info">
                             <h3 className="pub_number">Pub 4</h3>
                             <p className="pub_name">The Golden Lion</p>
                         </div>
                     </article>
-
+                    {/* a pub card, image and text description */}
                     <article className="pub_section">
                         <img src={pub5} className="pub_img" alt="Image of the front of a pub"></img>
-
                         <div className="pub_info">
                             <h3 className="pub_number">Pub 5</h3>
                             <p className="pub_name">Queens Head</p>
                         </div>
                     </article>
-
+                    {/* a pub card, image and text description */}
                     <article className="pub_section">
                         <img src={pub6} className="pub_img" alt="Image of the front of a pub"></img>
-
                         <div className="pub_info">
                             <h3 className="pub_number">Pub 6</h3>
                             <p className="pub_name">Styles Bar</p>
                         </div>
                     </article>
-
+                    {/* a pub card, image and text description */}
                     <article className="pub_section">
                         <img src={pub7} className="pub_img" alt="Image of the front of a pub"></img>
-
                         <div className="pub_info">
                             <h3 className="pub_number">Pub 7</h3>
                             <p className="pub_name">The Crickiters</p>
                         </div>
                     </article>
-
+                    {/* a pub card, image and text description */}
                     <article className="pub_section">
                         <img src={pub8} className="pub_img" alt="Image of the front of a pub"></img>
-
                         <div className="pub_info">
                             <h3 className="pub_number">Pub 8</h3>
                             <p className="pub_name">Liquid Bar</p>
                         </div>
                     </article>
-
+                    {/* a pub card, image and text description */}
                     <article className="pub_section">
                         <img src={pub9} className="pub_img" alt="Image of the front of a pub"></img>
-
                         <div className="pub_info">
                             <h3 className="pub_number">Pub 9</h3>
                             <p className="pub_name">The Court Leet</p>
@@ -105,7 +101,7 @@ export default function Activities(){
                     </article>
                 </div>
             </section>
-        </body>
+        </div>
         
     )
 }
